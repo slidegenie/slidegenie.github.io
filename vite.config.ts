78fcb9b11c3ad01b42b8904120b0f16a9fa5ffe9
dzldebby <debby.lingzl@gmail.com>
2025-03-31 15:47:35 +0800

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: 'slidegenie.github.io',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
